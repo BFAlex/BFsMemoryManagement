003c9090a444165d5210d7496bed63cf8d95100c
GKD <liuling@liulingdemac-mini.local>
2019-02-28 17:38:43 +0800

    20190228

---
+++ BFsMemoryManagement/BFsMemoryManagement/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BFsMemoryManagement
+//
+//  Created by 刘玲 on 2019/2/28.
+//  Copyright © 2019年 BFs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BFsMemoryManagement/BFsMemoryManagement/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BFsMemoryManagement
+//
+//  Created by 刘玲 on 2019/2/28.
+//  Copyright © 2019年 BFs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
